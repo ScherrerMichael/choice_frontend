579ff13433e691a7c43ce12d69ffd9db7febd708
ScherrerMichael <michaelscherrer23@gmail.com>
2021-10-10 20:44:35 -0700

    init event page

--- pages/events.js
+++ pages/events.js
@@ -1,1 +1,11 @@
 import React, { Component } from 'react';
+
+export default function Menu({ data }) {
+    return (
+        <>
+            <Header />
+            <div></div>
+            <Footer />
+        </>
+    );
+}
