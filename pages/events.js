392dd83f20927f6a8e8cc5ad0cba5711958ed321
ScherrerMichael <michaelscherrer23@gmail.com>
2021-10-10 20:48:16 -0700

    fixed event page init

--- pages/events.js
+++ pages/events.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Footer from './components/footer'
+import Header from './components/header'
 
 export default function Menu({ data }) {
     return (
