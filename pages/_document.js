6184f5e24e4fd7356642a34dca7ffcaa156b7e63
ScherrerMichael <michaelscherrer23@gmail.com>
2021-10-26 09:08:37 -0700

    added title

--- pages/_document.js
+++ pages/_document.js
@@ -7,9 +7,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-<link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet"/>
+          <title> Choice Deli in Ballard</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
