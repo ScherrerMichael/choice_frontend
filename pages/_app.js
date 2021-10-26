71663615222fe7b2bab22f575c58285c1c87a8bb
ScherrerMichael <michaelscherrer23@gmail.com>
2021-10-26 10:59:59 -0700

    fixed head tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles/styles.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-   return <Component {...pageProps} />
+   return(
+      <>
+   <Head>
+          <title> Choice Deli in Ballard</title>
+   </Head>
+   <Component {...pageProps} />
+   </>
+   )
 }
